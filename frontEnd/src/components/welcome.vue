3371680d235e1a024a19147536866d34d5b5690e
刘武松 <liuws@chingo.cn>
2017-12-01 13:26:46 +0800

    更新vue

---
+++ frontEnd/src/components/welcome.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="welcome">
+
+	</div>
+</template>
+
+<script>
+import VueHead from "./common/header";
+
+export default {
+    name: "Welcome",
+    components: {
+        VueHead
+    },
+    data() {
+        return {
+            msg: "Welcome to Your Vue.js App"
+        };
+    }
+};
+</script>
