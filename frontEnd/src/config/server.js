ac6adde60a2cfbd51930cc5b431f847e4f94fab7
刘武松 <liuws@chingo.cn>
2018-03-21 11:16:28 +0800

    跳转地址加判断

--- frontEnd/src/config/server.js
+++ frontEnd/src/config/server.js
@@ -2,9 +2,10 @@ const env = process.env.NODE_ENV;
 let serverUrl = "http://ckq.chingo.cn:8081/lab-inspect/php/index.php";
 const pathName = "";
 //前端登录地址
-const frontUrl = "http://192.168.240.81:8090/login";
+let frontUrl = "http://192.168.240.81:8090/login";
 if(env === "production"){
     serverUrl = "http://222.193.95.173/php/index.php";
+    frontUrl = "http://192.168.240.81:8090/login";
     // serverUrl = "http://lab-inspcet.appdemo.chingo.cn/php/index.php";
     // serverUrl = "http://192.168.99.103/site1/php/index.php";
 }
